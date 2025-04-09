0626c830df9aebc2ee99ed171299aac54fb442c9
Daniel <daniel0302acevedo@gmail.com>
2025-04-08 21:11:26 -0500

    Optimizando rutas para obtener productos por Id

--- routes/products.ts
+++ routes/products.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import productsController from '../controllers/products-controller';
 import getProducts from "../controllers/getProducts-controller";
+import productsId from "../controllers/productsId-controller";
 const router = express.Router();
 
 
 router.post('/', productsController);
 router.get('/', getProducts);
+router.get('/:id', productsId);
 
 export default router
